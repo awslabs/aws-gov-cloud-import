22db7a76fa3cf44f79f261c158c43d7fcdf2cc87
Jason Shaffer <37805841+awsjason@users.noreply.github.com>
2018-04-27 08:09:48 -0500

    Update gov-cloud-import-install.sh

--- gov-cloud-import-install.sh
+++ gov-cloud-import-install.sh
@@ -230,7 +230,7 @@ aws s3 sync ./ s3://gov-cloud-import-$RAND --profile com12345 --quiet
 
 echo "Deploying CloudFormations"
 #Start Cloudformation
-aws cloudformation create-stack --stack-name gov-cloud-import --template-body file://cloudformation/gov-cloud-import-commerical.json --capabilities CAPABILITY_IAM --profile com12345
+aws cloudformation create-stack --stack-name gov-cloud-import --template-body file://cloudformation/gov-cloud-import-commercial.json --capabilities CAPABILITY_IAM --profile com12345
 aws cloudformation create-stack --stack-name gov-cloud-import --template-body file://cloudformation/gov-cloud-import-govcloud.json --capabilities CAPABILITY_NAMED_IAM --profile gov12345
 
 #Watch Formations
