c8c9c1bd1dfd20d269bd7c05dd09607ea22decd3
Jason Shaffer <37805841+awsjason@users.noreply.github.com>
2018-11-26 10:17:55 -0600

    GovCloud East Update

--- gov-cloud-import-install.sh
+++ gov-cloud-import-install.sh
@@ -14,12 +14,12 @@
 ######################################################################################################################
 clear
 cat << "EOF"
-                            _                 _       _                            _   
-  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_ 
+                            _                 _       _                            _
+  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_
  / _` |/ _ \ \ / /____ / __| |/ _ \| | | |/ _` |_____| | '_ ` _ \| '_ \ / _ \| '__| __|
-| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_ 
+| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_
  \__, |\___/ \_/       \___|_|\___/ \__,_|\__,_|     |_|_| |_| |_| .__/ \___/|_|   \__|
- |___/                                                           |_|                   
+ |___/                                                           |_|
 
 Written by awsjason
 Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -48,12 +48,12 @@ read -p "Admin AWS Access Key: " COM_ACCESS_KEY
 read -p "Admin AWS Secret Key: " COM_SECRET_KEY
 clear
 cat << "EOF"
-                            _                 _       _                            _   
-  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_ 
+                            _                 _       _                            _
+  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_
  / _` |/ _ \ \ / /____ / __| |/ _ \| | | |/ _` |_____| | '_ ` _ \| '_ \ / _ \| '__| __|
-| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_ 
+| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_
  \__, |\___/ \_/       \___|_|\___/ \__,_|\__,_|     |_|_| |_| |_| .__/ \___/|_|   \__|
- |___/                                                           |_|                   
+ |___/                                                           |_|
 
 Written by awsjason
 Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -71,10 +71,12 @@ while :
     case $OPT in
       1) COM_REGION="us-west-2"
          GOV_REGION="us-gov-west-1"
+         CARD_DIR="west"
          break;;
 
       2) COM_REGION="us-east-2";
-         GOV_REGION="us-gov-east-2"
+         GOV_REGION="us-gov-east-1"
+         CARD_DIR="east"
          break;;
 
       *) echo "$OPT is an invaild option.";
@@ -108,21 +110,21 @@ while :
 #Set the creds
 aws configure set aws_access_key_id $GOV_ACCESS_KEY --profile gov12345
 aws configure set aws_secret_access_key $GOV_SECRET_KEY --profile gov12345
-aws configure set region us-gov-west-1 --profile gov12345
+aws configure set region $GOV_REGION --profile gov12345
 aws configure set output text --profile gov12345
 
 aws configure set aws_access_key_id $COM_ACCESS_KEY --profile com12345
 aws configure set aws_secret_access_key $COM_SECRET_KEY --profile com12345
-aws configure set region us-west-2 --profile com12345
+aws configure set region $COM_REGION --profile com12345
 aws configure set output text --profile com12345
 clear
 cat << "EOF"
-                            _                 _       _                            _   
-  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_ 
+                            _                 _       _                            _
+  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_
  / _` |/ _ \ \ / /____ / __| |/ _ \| | | |/ _` |_____| | '_ ` _ \| '_ \ / _ \| '__| __|
-| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_ 
+| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_
  \__, |\___/ \_/       \___|_|\___/ \__,_|\__,_|     |_|_| |_| |_| .__/ \___/|_|   \__|
- |___/                                                           |_|                   
+ |___/                                                           |_|
 
 Written by awsjason
 Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -130,19 +132,30 @@ Licensed under the Amazon Software License (the "License"). You may not
 use this file except in compliance with the License.
 EOF
 echo ""
-echo "Thank you. Checking for Previous Install."
+echo "Thank you. Checking for Previous Installations elements."
 echo ""
-echo "INFO: AWS SDK Will show 2 errors if no installation exists."
-COM_STATUS=$(aws cloudformation describe-stacks --stack-name gov-cloud-import --profile com12345 | awk 'NF>1{print $NF}' | head -n 1)
-GOV_STATUS=$(aws cloudformation describe-stacks --stack-name gov-cloud-import --profile gov12345 | awk 'NF>1{print $NF}' | head -n 1)
-if [ "$GOV_STATUS" != "" ] || [ "$COM_STATUS" != "" ]; then
+echo ""
+COM_STATUS=$(aws cloudformation describe-stacks --stack-name gov-cloud-import --profile com12345 2> null | awk 'NF>1{print $NF}' | head -n 1)
+GOV_STATUS=$(aws cloudformation describe-stacks --stack-name gov-cloud-import --profile gov12345 2> null | awk 'NF>1{print $NF}' | head -n 1)
+ROLE_STATUS=$(aws iam get-role --role-name vmimport --profile gov12345 2> null | awk '/vmie.amazonaws.com/ {print $2}')
+
+if [ "$GOV_STATUS" != "" ] || [ "$COM_STATUS" != "" ] || [ "$ROLE_STATUS" != "" ]; then
+  if [[ -z ${GOV_STATUS} ]]; then
+      GOV_STATUS="Does not exist"
+  fi
+  if [[ -z ${COM_STATUS} ]]; then
+      COM_STATUS="Does not exist"
+  fi
+  if [[ -z ${ROLE_STATUS} ]]; then
+      ROLE_STATUS="Does not exist"
+  fi
   echo ""
   echo "Previous Cloudformation detected:"
   echo ""
   echo "$GOV_REGION status: $GOV_STATUS"
   echo "$COM_REGION status: $COM_STATUS"
-  sleep 20
-  exit
+  echo "VMImport Role status: $ROLE_STATUS"
+  sleep 5
 else
   echo ""
   echo "No Cloudformation detected in $GOV_REGION or $COM_REGION."
@@ -151,12 +164,12 @@ fi
 
 clear
 cat << "EOF"
-                            _                 _       _                            _   
-  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_ 
+                            _                 _       _                            _
+  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_
  / _` |/ _ \ \ / /____ / __| |/ _ \| | | |/ _` |_____| | '_ ` _ \| '_ \ / _ \| '__| __|
-| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_ 
+| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_
  \__, |\___/ \_/       \___|_|\___/ \__,_|\__,_|     |_|_| |_| |_| .__/ \___/|_|   \__|
- |___/                                                           |_|                   
+ |___/                                                           |_|
 
 Written by awsjason
 Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
@@ -168,7 +181,9 @@ echo "Supplied API Keys will be removed upon completion."
 echo ""
 echo "Starting gov-cloud-import deployment to $GOV_REGION and $COM_REGION."
 echo ""
-
+echo ""
+echo "Making S3 Bucket in $COM_REGION"
+echo ""
 #Make Bucket
 RAND=$(openssl rand -base64 100 | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
 
@@ -198,14 +213,17 @@ rm policy.json
 #Save Bucket As Parameter
 aws ssm put-parameter --name "gov-cloud-import-app" --type "String" --value "gov-cloud-import-$RAND" --overwrite --profile com12345 > /dev/null
 
+echo "Copying App to S3 Bucket in $COM_REGION"
+echo ""
 #Sync the local repo clone to new s3 buckets
 aws s3 sync ./ s3://gov-cloud-import-$RAND --quiet  --profile com12345
 
 #Update Scripts with New Bucket Location
-BUCKET_BASE_URL="https://s3.$COM_REGION.amazonaws.com/gov-cloud-import-$RAND"
+#BUCKET_BASE_URL="https://s3.$COM_REGION.amazonaws.com/gov-cloud-import-$RAND"
 BUCKET_BASE="gov-cloud-import-$RAND"
-sed -ie "s|<BUCKET_BASE_URL>|$BUCKET_BASE_URL|g" ec2/gov-cloud-import-ec2prep.sh
+BUCKET_BASE_URL="https://s3.$COM_REGION.amazonaws.com/gov-cloud-import-$RAND"
 
+sed -ie "s|<BUCKET_BASE>|$BUCKET_BASE|g" ec2/gov-cloud-import-ec2prep.sh
 sed -ie "s|<BUCKET_BASE>|$BUCKET_BASE|g" cloudformation/gov-cloud-import-commercial.json
 
 #Zip up lambda files
@@ -219,7 +237,7 @@ do
       echo "Root Directory"
     else
       cd $i
-      zip -X ../$i.zip index.js 
+      zip -X ../$i.zip index.js
       cd ../
     fi
 done
@@ -232,8 +250,10 @@ aws s3 sync ./ s3://gov-cloud-import-$RAND --profile com12345 --quiet
 echo "Deploying CloudFormations"
 #Start Cloudformation
 aws cloudformation create-stack --stack-name gov-cloud-import --template-body file://cloudformation/gov-cloud-import-commercial.json --capabilities CAPABILITY_IAM --profile com12345
-aws cloudformation create-stack --stack-name gov-cloud-import --template-body file://cloudformation/gov-cloud-import-govcloud.json --capabilities CAPABILITY_NAMED_IAM --profile gov12345
-
+aws cloudformation create-stack --stack-name gov-cloud-import --template-body file://cloudformation/gov-cloud-import-govcloud-$CARD_DIR.json --capabilities CAPABILITY_NAMED_IAM --profile gov12345
+if [ "$ROLE_STATUS" != "vmie.amazonaws.com" ]; then
+  aws cloudformation create-stack --stack-name gov-cloud-import-vmimport --template-body file://cloudformation/gov-cloud-import-govcloud-vmimport.json --capabilities CAPABILITY_NAMED_IAM --profile gov12345
+fi
 #Watch Formations
 echo ""
 echo "Script will check Cloudformation status every 30 seconds until complete."
@@ -266,10 +286,10 @@ done
 #Make AWS GovCloud (US) API Keys
 echo ""
 echo "Creating AWS GovCloud (US) Access Keys and Storing them in AWS SSM Parameters."
-CREATE_KEYS=$(aws iam create-access-key --user-name gov-cloud-import-user --profile gov12345)
+CREATE_KEYS=$(aws iam create-access-key --user-name gov-cloud-import-user-$CARD_DIR --profile gov12345)
 ACCESS_KEY=$(echo $CREATE_KEYS | awk '{print $2}')
 SECRET_KEY=$(echo $CREATE_KEYS | awk '{print $4}')
-GOV_BUCKET=$(aws cloudformation describe-stacks --stack-name gov-cloud-import --profile gov12345 | awk 'NF>1{print $NF}' | sed '3q;d')
+GOV_BUCKET=$(aws cloudformation describe-stacks --stack-name gov-cloud-import --profile gov12345 | awk '/gov-cloud-import-bucket*/ {print $2}' | tr -d '"')
 
 #Submit Parameters to AWS
 aws ssm put-parameter --name "gov-cloud-import-accessKey" --overwrite  --type "SecureString" --value $ACCESS_KEY --profile com12345 > /dev/null
@@ -279,13 +299,13 @@ aws ssm put-parameter --name "gov-cloud-import-s3Bucket" --overwrite  --type "St
 #Remove Creds/Keys and Config
 clear
 cat << "EOF"
-                            _                 _       _                            _   
-  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_ 
+                            _                 _       _                            _
+  __ _  _____   __      ___| | ___  _   _  __| |     (_)_ __ ___  _ __   ___  _ __| |_
  / _` |/ _ \ \ / /____ / __| |/ _ \| | | |/ _` |_____| | '_ ` _ \| '_ \ / _ \| '__| __|
-| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_ 
+| (_| | (_) \ V /_____| (__| | (_) | |_| | (_| |_____| | | | | | | |_) | (_) | |  | |_
  \__, |\___/ \_/       \___|_|\___/ \__,_|\__,_|     |_|_| |_| |_| .__/ \___/|_|   \__|
- |___/                                                           |_|                   
-                                       _                            _
+ |___/                                                           |_|
+
 Written by awsjason
 Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Amazon Software License (the "License"). You may not
